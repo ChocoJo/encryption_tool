4b867cdcb8794d010fac13cd86bd33ab5c77670f
Johanna Hailemichael <johanna.hailemichael@iths.se>
2024-10-22 15:52:50 +0200

    Add logging setup for crypto tool

--- crypto_tool.py
+++ crypto_tool.py
@@ -3,3 +3,6 @@ import logging
 import argparse
 from cryptography.fernet import Fernet
 
+logging.basicConfig(level=logging.INFO, 
+                    format='%(asctime)s - %(levelname)s - %(message)s'
+)
