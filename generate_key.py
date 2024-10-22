0b529b4462e3c3ac69c36f73d46fffe136455502
Johanna Hailemichael <johanna.hailemichael@iths.se>
2024-10-22 15:42:57 +0200

    Add logging setup for key generation

--- generate_key.py
+++ generate_key.py
@@ -3,3 +3,8 @@ import sys
 import logging
 from cryptography.fernet import Fernet
 import argparse
+
+logging.basicConfig(
+    level=logging.INFO, 
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
